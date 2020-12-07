03dd54785db98851aa638169b873b4da504282f3
deanbgarlick <deanbgarlick@gmail.com>
2020-12-07 10:22:21 -0800

    Remove unnecessary commented line

--- flask/app/config.py
+++ flask/app/config.py
@@ -5,6 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 'sqlite:///' + os.path.join(basedir, 'app.db'))
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', "dev mode secret key")
